b26ff7505ecf35cb2697ef946a6507f8253cbbaf
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-10-02 01:09:38 +0200

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+
+import AddTutorial from "./components/add-tutorial";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorial-list";
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
+          <Link to={"/tutorials"} className="navbar-brand">
             bezKoder
-          </a>
+          </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
               <Link to={"/tutorials"} className="nav-link">
@@ -26,10 +31,10 @@ class App extends Component {
 
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<TutorialsList />} />
-            <Route path="/tutorials" element={<TutorialsList />} />
-            <Route path="/add" element={<AddTutorial />} />
-            <Route path="/tutorials/:id" element={<Tutorial />} />
+            <Route path="/" element={<TutorialsList/>} />
+            <Route path="/tutorials" element={<TutorialsList/>} />
+            <Route path="/add" element={<AddTutorial/>} />
+            <Route path="/tutorials/:id" element={<Tutorial/>} />
           </Routes>
         </div>
       </div>
