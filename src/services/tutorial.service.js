654b2b0a9449b8445cfefd07078261ef468ae56a
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-10-06 11:31:24 +0200

    seventh commit

--- src/services/tutorial.service.js
+++ src/services/tutorial.service.js
@@ -2,7 +2,7 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("http://localhost:8080/api/tutorials");
+    return http.get("https://tutorialapp.onrender.com//api/tutorials");
   }
 
   // get(id) {
@@ -10,7 +10,7 @@ class TutorialDataService {
   // }
 
   get(id) {
-    return http.get(`http://localhost:8080/api/tutorials/${id}`)
+    return http.get(`https://tutorialapp.onrender.com/api/tutorials/${id}`)
      .catch(error => {
       console.error("Error fetching tutorial:", error);
       throw error;
@@ -18,23 +18,23 @@ class TutorialDataService {
   }
 
   create(data) {
-    return http.post("http://localhost:8080/api/tutorials", data);
+    return http.post("https://tutorialapp.onrender.com/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`http://localhost:8080/api/tutorials/${id}`, data);
+    return http.put(`https://tutorialapp.onrender.com/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`http://localhost:8080/api/tutorials/${id}`);
+    return http.delete(`https://tutorialapp.onrender.com/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`http://localhost:8080/api/tutorials`);
+    return http.delete(`https://tutorialapp.onrender.com/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`http://localhost:8080/api/tutorials?title=${title}`);
+    return http.get(`https://tutorialapp.onrender.com/api/tutorials?title=${title}`);
   }
 }
 
